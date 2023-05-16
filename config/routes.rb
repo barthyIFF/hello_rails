cba26df090dbf2c630613f93c57a7d003d6b4c75
Claudinho <barthy@gmail.com>
2023-05-16 18:37:38 -0300

    Criando listagem, edicao e criacao de usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  #get 'users/index'
+  #get 'users/show'
+  #get 'users/new'
+  #get 'users/edit'  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users
-  get 'about' => 'pages#about'  
+
+  get 'about' => 'pages#about'
   get 'index' => 'pages#index'
-  root to: 'pages#index'
+  get 'newUser' => 'pages#newUser'
+  root to: 'pages#about'
+  devise_for :users, :skip => [:registrations]
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+  resources :users
 end
